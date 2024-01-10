475897bb368cc9525d6b453fb56bbb80083741da
Genevieve Strittmatter <eastrittmatter@gmail.com>
2024-01-10 16:34:41 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(driverNames, nameToFind) {
+    return driverNames.filter(driverName => driverName.toUpperCase() === nameToFind.toUpperCase());
+}
+
+function fuzzyMatch(driverNames, desiredStartingCharacters) {
+    return driverNames.filter(driverName => driverName.toUpperCase().startsWith(desiredStartingCharacters.toUpperCase()));
+}
+
+function matchName(driverData, nameToFind) {
+    return driverData.filter(singleDriver => singleDriver.name.toUpperCase() === nameToFind.toUpperCase());
+}
